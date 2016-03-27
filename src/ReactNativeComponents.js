cc493a1a9c848a743c9e95a1280f2174ad7225ef
kiki le singe <aalbertini95@gmail.com>
2016-03-27 23:44:51 +0200

    feat(ReactNativeComponents): implement Navigation component

---
+++ src/ReactNativeComponents.js
@@ -0,0 +1,13 @@
+import React, { Component, View } from 'react-native';
+
+import Navigation from './components/Navigation';
+
+// http://blog.paracode.com/2016/01/05/routing-and-navigation-in-react-native/
+
+export default class ReactNativeComponents extends Component {
+  render() {
+    return (
+      <Navigation />
+    );
+  }
+}
