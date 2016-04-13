660a8837b7ac388fe7dd13a389273e0fec6e35f9
kiki le singe <aalbertini95@gmail.com>
2016-04-13 22:53:59 +0200

    feat(Navigation): implement navigationBar component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,6 +3,62 @@ import React, { StyleSheet, Component, Navigator } from 'react-native';
 import Scene1 from '../../scenes/Scene1';
 import Scene2 from '../../scenes/Scene2';
 import Scene3 from '../../scenes/Scene3';
+import LeftButton from './LeftButton';
+import RightButton from './RightButton';
+import NavigationBarTitle from './NavigationBarTitle';
+
+const NavigationBarRouteMapper = {
+  LeftButton: (route, navigator, index, navState) => {
+    // console.log('LeftButton');
+    // console.log(route);
+    // console.log(navigator);
+    // console.log(index);
+    // console.log(navState);
+    // console.log('LeftButton');
+
+    if (index === 0) {
+      return null;
+    }
+
+    // const previousRoute = navState.routeStack[index - 1];
+    return (
+      <LeftButton
+        onBack={() => {
+          navigator.pop();
+        }}
+      />
+    );
+  },
+
+  RightButton: (route, navigator, index, navState) => {
+    const numberScene = index + 2;
+
+    // console.log('RightButton');
+    // console.log(route);
+    // console.log(navigator);
+    // console.log(index);
+    // console.log(navState);
+    // console.log('RightButton');
+
+    if (index === 2) {
+      return null;
+    }
+
+    return (
+      <RightButton
+        onNext={() => {
+          navigator.push({ id: `scene${numberScene}` });
+        }}
+      />
+    );
+  },
+
+  Title: (route, navigator, index, navState) => {
+    return (
+      <NavigationBarTitle />
+    );
+  },
+};
 
 export default class Navigation extends Component {
   renderScene(route, navigator) {
@@ -25,6 +81,12 @@ export default class Navigation extends Component {
         style={ styles.navigator }
         sceneStyle={ styles.scene }
         renderScene={ this.renderScene }
+        navigationBar={
+          <Navigator.NavigationBar
+            style={ styles.navigationBar }
+            routeMapper={ NavigationBarRouteMapper }
+          />
+        }
       />
     );
   }
@@ -36,5 +98,10 @@ const styles = StyleSheet.create({
   },
   scene: {
     padding: 5,
+  },
+  navigationBar: {
+    backgroundColor: '#5B29C1',
+    borderBottomColor: '#48209A',
+    borderBottomWidth: 1,
   },
 });
